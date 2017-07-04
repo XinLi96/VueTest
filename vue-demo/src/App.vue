2c6575a14eb018de7296a5cf66cf82c8ba85e11b
duanzhaopeng <duan123qwe@163.com>
2017-07-04 14:28:38 +0800

    Vue试炼-创建项目+组件之间简单传值

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <child v-bind:message="parentMsg" v-on:listenToChildEvent="showMsgFromChild"></child>
+  </div>
+</template>
+
+<script>
+import child from './components/Child'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      parentMsg:"hello,child"
+    }
+  },
+  methods:{
+    showMsgFromChild:function(data){
+      alert(data);
+    }
+  },
+  components: {
+    child
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-demo/src/components/Child.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h2>child子组件部分</h2>
+		<p>{{message}}</p>
+		<button v-on:click="sendMsgToParent">向父组件传值</button>
+	</div>
+</template>
+<script>
+	export default{
+		props:["message"],
+		methods:{
+			sendMsgToParent:function(){
+				this.$emit("listenToChildEvent","this message is from child");
+			}
+		}
+	}
+</script>
