c53aca1bc8d38b67f07501eac5bd1b81253eb5c8
XinLi96 <291750956@qq.com>
2017-09-26 17:15:58 +0800

    17/9/26 17.15

--- assets/js/setUp.js
+++ assets/js/setUp.js
@@ -1,6 +1,10 @@
 var oSub = $('#sub');
 
 oSub.click(function(){
-    alert('保存成功');
+    // alert('保存成功');
+    $('#popUp').removeClass('yc');
+    setTimeout(function(){
+        $('#popUp').addClass('yc');
+    },1000);
     $('#listNow').append("<tr>< <td>收藏夹</td><td>普通商务、高级商务、资深商务、客户顾问、客户主任</td> <td>正式、试用</td><td>200</td><td>2</td><td>2</td></tr>")
 });
