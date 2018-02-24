791f03219ed07cbc6e6a78f0bad3b8786ae1b885
XinLi96 <291750956@qq.com>
2018-02-24 19:06:23 +0800

    美食管理

--- food/js/index.js
+++ food/js/index.js
@@ -1,4 +1,11 @@
 (function($){
+    var mySwiper = new Swiper('.swiper-container', {
+        autoplay: 2000,//可选选项，自动滑动
+        prevButton:'.swiper-button-prev',
+        nextButton:'.swiper-button-next',
+        loop : true,
+    })
+
     $('.rule').on('click',function(){
         $('.ruleMask').removeClass('toHide');
     });
